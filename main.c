6a70b383adbfd6197a4e12f20448ef27631a6a63
Juan Casilla <casillajec@gmail.com>
2021-02-11 07:20:28 -0400

    improved input handling to keep tack of currently pressed key

--- main.c
+++ main.c
@@ -73,52 +73,56 @@ float get_hz(char note, char mod, int octave) {
 	return 440.0 * hz_diff;
 }
 
-void process_input(char* note, char* mod, int* octave, int* quit_requested) {
+void process_input(SDL_Scancode* pressed_key, char* note, char* mod, int* octave, int* quit_requested) {
 	SDL_Event evt;
-	//if (SDL_HasEvent(SDL_KEYDOWN) == SDL_FALSE) { note = 'S'; mod = ' '; }
 	while (SDL_PollEvent(&evt)) {
-		if 	(evt.type == SDL_QUIT) { *quit_requested = 1; }
-		else if (evt.type == SDL_KEYDOWN) {
-			switch (evt.key.keysym.scancode) {
-				case SDL_SCANCODE_ESCAPE:
-					*quit_requested = 1; break;
-				case SDL_SCANCODE_Z:
-					*note = 'A'; *mod = ' '; break;
-				case SDL_SCANCODE_S:
-					*note = 'A'; *mod = '#'; break;
-				case SDL_SCANCODE_X:
-					*note = 'B'; *mod = ' '; break;
-				case SDL_SCANCODE_C:
-					*note = 'C'; *mod = ' '; break;
-				case SDL_SCANCODE_F:
-					*note = 'C'; *mod = '#'; break;
-				case SDL_SCANCODE_V:
-					*note = 'D'; *mod = ' '; break;
-				case SDL_SCANCODE_G:
-					*note = 'D'; *mod = '#'; break;
-				case SDL_SCANCODE_B:
-					*note = 'E'; *mod = ' '; break;
-				case SDL_SCANCODE_N:
-					*note = 'F'; *mod = ' '; break;
-				case SDL_SCANCODE_J:
-					*note = 'F'; *mod = '#'; break;
-				case SDL_SCANCODE_M:
-					*note = 'G'; *mod = ' '; break;
-				case SDL_SCANCODE_K:
-					*note = 'G'; *mod = '#'; break;
-				case SDL_SCANCODE_2:
-					*octave = 2; break;
-				case SDL_SCANCODE_3:
-					*octave = 3; break;
-				case SDL_SCANCODE_4:
-					*octave = 4; break;
-				case SDL_SCANCODE_5:
-					*octave = 5; break;
-				case SDL_SCANCODE_6:
-					*octave = 6; break;
+		if 	(evt.type == SDL_QUIT)
+			*quit_requested = 1;
+		else if (evt.type == SDL_KEYDOWN)
+			*pressed_key = evt.key.keysym.scancode; 
+		else if (evt.type == SDL_KEYUP && evt.key.keysym.scancode == *pressed_key)
+			*pressed_key = -1;
+	}
 
-			}
-		}
+	switch (*pressed_key) {
+		case SDL_SCANCODE_ESCAPE:
+			*quit_requested = 1; break;
+		case SDL_SCANCODE_Z:
+			*note = 'A'; *mod = ' '; break;
+		case SDL_SCANCODE_S:
+			*note = 'A'; *mod = '#'; break;
+		case SDL_SCANCODE_X:
+			*note = 'B'; *mod = ' '; break;
+		case SDL_SCANCODE_C:
+			*note = 'C'; *mod = ' '; break;
+		case SDL_SCANCODE_F:
+			*note = 'C'; *mod = '#'; break;
+		case SDL_SCANCODE_V:
+			*note = 'D'; *mod = ' '; break;
+		case SDL_SCANCODE_G:
+			*note = 'D'; *mod = '#'; break;
+		case SDL_SCANCODE_B:
+			*note = 'E'; *mod = ' '; break;
+		case SDL_SCANCODE_N:
+			*note = 'F'; *mod = ' '; break;
+		case SDL_SCANCODE_J:
+			*note = 'F'; *mod = '#'; break;
+		case SDL_SCANCODE_M:
+			*note = 'G'; *mod = ' '; break;
+		case SDL_SCANCODE_K:
+			*note = 'G'; *mod = '#'; break;
+		case SDL_SCANCODE_2:
+			*octave = 2; break;
+		case SDL_SCANCODE_3:
+			*octave = 3; break;
+		case SDL_SCANCODE_4:
+			*octave = 4; break;
+		case SDL_SCANCODE_5:
+			*octave = 5; break;
+		case SDL_SCANCODE_6:
+			*octave = 6; break;
+		default:
+			*note = 'S'; *mod = ' '; break;
 	}
 }
 
@@ -191,11 +195,11 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	
-	SDL_Event evt;	
 	int quit_requested = 0;
+	SDL_Scancode pressed_key;
 	SDL_PauseAudioDevice(dev_id, 0);
 	while (!quit_requested) {
-		process_input(&note, &mod, &octave, &quit_requested);
+		process_input(&pressed_key, &note, &mod, &octave, &quit_requested);
 		cb_cont.kb_hz = get_hz(note, mod, octave);
 	}
 	beep_it_free(beep_it);
