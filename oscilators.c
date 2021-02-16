f1ae95d38186499ef9c7556d49f8e2e39e93f8fb
Juan Casilla <casillajec@gmail.com>
2021-02-16 13:19:04 -0400

    normalized oscilator functions to be on the range [-1...1]

--- oscilators.c
+++ oscilators.c
@@ -3,6 +3,7 @@
 #include "oscilators.h"
 
 const float TAU = 2.0 * M_PI;
+const float PI_HALF = M_PI / 2.0f;
 
 float osc_f(float pitch, float time, int osc, int pitch_mod, float mod_freq, float mod_amplitude) {
 	float output;
@@ -14,14 +15,14 @@ float osc_f(float pitch, float time, int osc, int pitch_mod, float mod_freq, flo
 			output = sin(freq);
 			break;
 		case OSC_SQUARE:
-			output = sin(freq) <= 0 ? -1.0f : 1.0f;
+			output = sin(freq) <= 0 ? -0.6f : 0.6f;
 			break;
 		case OSC_TRIANGLE:
-			output = asin(sin(freq));
+			output = asin(sin(freq)) / PI_HALF;
 			break;
 		case OSC_EXP:
 			output = sin(freq);
-			output = pow(output, 20) + pow(output, 25);
+			output = (pow(output, 20) + pow(output, 25)) / 2;
 			break;
 		default:
 			output = 0.0f;
